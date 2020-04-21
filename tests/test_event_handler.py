1f22b7c9417139c117d75fc1bab2aa823f946938
Pavel Altynnikov <Robenzon11@gmail.com>
2020-04-21 13:02:06 +0400

    (update) test_event_handler

--- tests/test_event_handler.py
+++ tests/test_event_handler.py
@@ -4,56 +4,14 @@ clr.AddReference('System.Windows.Forms')
 from System.Windows.Forms import Form, Button
 clr.AddReference('System.Drawing')
 from System.Drawing import Size, Point
-from abc import abstractmethod
-
-def foo():
-    print(123)
-
-
-a = foo
-a()
-
-
-class Test():
-    def __init__(self, function):
-        self.b = function
-
-    def function(self):
-        print("Я из класса Test")
-
-
-t = Test(foo)
-t.b()
-
-
-class Test2():
-    def __init__(self, another_class):
-        self.handler = another_class.function
-
-
-t2 = Test2(t)
-t2.handler()
-
-
-class Test3():
-    def __init__(self, another_class):
-        # self.handler_method = another_class.function
-        another_class.function = self.handler_method
-
-    def handler_method(self):
-        pass
-
-    def handler_method(self):
-        print("Я из Test3")
-
-
-t3 = Test3(t)
-# t3.handler_method()
-t.function()
 
 
 class Model():
-    pass
+    def __init__(self):
+        self._data = "data from model"
+
+    def print_data(self, sender, args):
+        print(self._data)
 
 
 class View(Form):
@@ -65,17 +23,13 @@ class View(Form):
         self._button.Location = Point(30, 20)
         self.CenterToScreen()
 
-        self.button_start_event_handler = self._button.Click
+    def delegate_button_click(self, method):
+        self._button.Click += method
 
 
 class Presenter():
     def __init__(self, form, model):
-        form.button_start_event_handler += self.function_handler
-
-    def function_handler(self, sender, args):
-        print("Я из Presenter")
-        print(sender)
-        print(args)
+        form.delegate_button_click(model.print_data)
 
 
 view = View()
